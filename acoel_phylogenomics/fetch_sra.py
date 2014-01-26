6fa22f3dc34f72a0b6cde8cea1a4a80c2bf9827b
Bruno Vellutini <organelas@gmail.com>
2014-01-26 02:17:46 -0500

    Major refactoring of the script
    Now that the function is more clear I adjusted the code. Set to work as
    a standalone script and a module. Objects organized in classes that can
    be instantiated independently if needed. Consists of a search and a
    package class and a main function for the command line. It still only
    fetches the summary; soon it will fetch the full metadata record.

--- acoel_phylogenomics/fetch_sra.py
+++ acoel_phylogenomics/fetch_sra.py
@@ -1,9 +1,15 @@
-import re
-from Bio import Entrez
-
+#!/usr/bin/env python
 '''Utility to filter and download SRA records.'''
 
-def extract_summary(summary):
+import argparse
+import re
+import sys
+
+from Bio import Entrez
+
+
+# TODO Adapt this function to full record inside SRAPackage class.
+def extract_summary(summary_string):
     '''Use regex to parse summary fields.
     <Summary>
         <Title>Transcriptome Analysis of house mouse</Title>
@@ -62,73 +68,184 @@ def extract_summary(summary):
     return summary_fields
 
 
-# Needed email.
-Entrez.email = 'organelas@gmail.com'
+class SRADatabase:
+    '''General information about SRA Database'''
 
-# Terms for query.
-search_terms = '((((("strategy rna seq"[Properties]) AND "platform illumina"[Properties]) AND metazoa[Organism]) NOT vertebrata[Organism]) NOT insects[Organism]) AND ("2000/01/01"[Modification Date] : "3000"[Modification Date])'
+    def __init__(self):
+        einfo_handle = Entrez.einfo(db='sra')
+        einfo = Entrez.read(einfo_handle, validate=False)
 
-# First search for records matching the terms.
-search_handle = Entrez.esearch(db='sra', term=search_terms, retmax=20)
-search_records = Entrez.read(search_handle)
+        # Define attributes.
+        self.count = einfo['DbInfo']['Count']
+        self.last_update = einfo['DbInfo']['LastUpdate']
+        self.menu_name = einfo['DbInfo']['MenuName']
+        self.description = einfo['DbInfo']['Description']
+        self.link_list = einfo['DbInfo']['LinkList']
+        self.field_list = einfo['DbInfo']['FieldList']
+        self.einfo = einfo
 
-# Print some information for the records.
-print('Found %s entries' % search_records['Count'])
-print('Search terms: %s' % search_records['QueryTranslation'])
+        #print(self.count, self.last_update, self.menu_name, self.description,
+              #self.link_list, self.field_list, self.einfo)
 
-# Save list with IDs.
-sra_ids = search_records['IdList']
+    # TODO Use def __unicode__ or __str__ to identify class objects.
 
-# Dictionary for summaries.
-sra_summaries = {}
+class SRASearch:
+    '''Perform search and keep IDs of SRA packages.
 
-# Fetch basic infos to filter entries.
-for sra_id in sra_ids:
-    summary_handle = Entrez.esummary(db='sra', id=sra_id)
-    summary_record = Entrez.read(summary_handle)
-    sra_summaries[sra_id] = summary_record
+    Example of query:
 
-# Paired or single reads will be parsed from the field ExpXml. Examples:
-# paired: http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&id=445724
-# single: http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&id=423751
+        ((((("strategy rna seq"[Properties]) AND "platform illumina"[Properties])
+        AND metazoa[Organism]) NOT vertebrata[Organism]) NOT insects[Organism]) AND
+        ("2000/01/01"[Modification Date] : "3000"[Modification Date])
+    '''
 
-# Create list for selected IDs.
-sra_selected_ids = []
+    def __init__(self, query, retmax, email):
+        # Required arguments.
+        self.query = query
+        if int(retmax) > 100000:
+            # Limit defined by Entrez.
+            self.retmax = 100000
+        else:
+            self.retmax = retmax
+        Entrez.email = email
 
-# Write IDs and READ LENGTH to CSV file.
-paired_sra = open('paired_sra.csv', 'w')
+        # Search metadata.
+        self.count = None
+        self.retstart = None
+        self.query_translation = None
+        self.idlist = None
 
-# Iterate through summaries and pick paired end datasets.
-header = ['SRA_ID', 'ORGANISM', 'NCBI_TAXID', 'STRATEGY', 'PLATFORM', 'TYPE',
-          'READ_LENGTH', 'READS', 'SPOTS', 'BASES']
+        # Additional attributes.
+        self.results = None
+        self.database = SRADatabase()
+        # TODO Add timestamp.
 
-paired_sra.write(', '.join(header) + '\n')
-print('\n' + '\t '.join(header))
-for k, v in sra_summaries.iteritems():
+    def esearch(self):
+        '''Search SRA packages with Entrez using query.'''
+        handle = Entrez.esearch(db='sra', term=self.query, retmax=self.retmax)
+        self.results = Entrez.read(handle)
+        self.parse_results()
+        return self.results
 
-    # Pattern to match: <PAIRED NOMINAL_LENGTH="200"
-    summary_string = v[0]['ExpXml']
+    def parse_results(self):
+        '''Populate class attributes by parsing results.'''
+        self.count = self.results['Count']
+        self.retstart = self.results['RetStart']
+        self.query_translation = self.results['QueryTranslation']
+        self.idlist = self.results['IdList']
 
-    # Extract fields from record summary.
-    fields = extract_summary(summary_string)
+        #print(self.count, self.retstart, self.query_translation, self.idlist)
 
-    sra_selected_ids.append(k)
-    row = [k, fields['organism'], fields['ncbi_taxid'], fields['strategy'],
-            fields['platform'], fields['type'] ,fields['length'],
-            fields['reads'], fields['spots'], fields['spots']]
-    if fields['type'] == 'PAIRED':
-        paired_sra.write(', '.join(row) + '\n')
-    print('\t '.join(row))
+class SRAPackage:
+    '''Fetch and store metadata from a SRA package.'''
 
-# Closes CSV file.
-paired_sra.close()
+    def __init__(self, sra_id):
+        self.id = sra_id
+        self.summary = None
+        self.fields = None
 
-# Just count selected IDs.
-count_sra_selected_ids = len(sra_selected_ids)
+        self.efetch()
 
-print('\nTotal of %d selected IDs. %d%% of returned entries.' % (count_sra_selected_ids, count_sra_selected_ids / int(search_records['Count'])))
+    def efetch(self):
+        '''Fetch package metadata from Entrez'''
+        # TODO Use efetch instead of esummary to get read_length correctly.
+        handle = Entrez.esummary(db='sra', id=self.id)
+        self.summary = Entrez.read(handle)
+        self.extract()
 
-# TODO Use IDs to fetch the full records of these. Apparently the only return
-# type is XML, so it needs to be parsed to get a tabular output, if needed.
+    def extract(self):
+        '''Extract relevant fields from summary.'''
+        # TODO Make a real function and define attribute fields individually
+        # from full record metadata.
+        string = self.summary[0]['ExpXml']
+        self.fields = extract_summary(string)
 
-# TODO find out how to fetch the sequences, iteratively by ID?
+        #print(self.fields)
+
+
+class FilterPackages:
+    '''Filter results based on package metadata.'''
+    # TODO Plan a way to effectively filter results. Maybe get scipy or pandas
+    # help?
+
+    def __init__(self, packages):
+        self.packages = packages
+
+
+def main():
+    '''Parse arguments and call SRA search.
+
+    Main function simply parses arguments from command line input and assures
+    everything is ok to instantiate the SRA search class.
+    '''
+
+    # Parse arguments.
+    parser = argparse.ArgumentParser(description='Search & Fetch records from NCBI\'s Sequence Read Archive.',
+                                     epilog='Work out those reads, dude.')
+    parser.add_argument('-s', '--search',
+                        help='put search terms between "quotes"',
+                        type=str, required=True)
+    parser.add_argument('-m', '--maximum',
+                        help='maximum number of records to be retrieved',
+                        default='20')
+    parser.add_argument('-o', '--output',
+                        help='indicate output CSV file',
+                        required=True)
+    parser.add_argument('-e', '--email',
+                        help='an email address is required for Entrez',
+                        required=True)
+    args = parser.parse_args()
+
+    # Instantiate search object.
+    sra_search = SRASearch(query=args.search, retmax=args.maximum,
+                           email=args.email)
+
+    # Execute search itself.
+    sra_search.esearch()
+
+    # Fetch metadata from packages.
+    packages = [SRAPackage(sra_id) for sra_id in sra_search.idlist]
+
+if __name__ == '__main__':
+    main()
+
+
+## Paired or single reads will be parsed from the field ExpXml. Examples:
+## paired: http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&id=445724
+## single: http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&id=423751
+
+## Create list for selected IDs.
+#sra_selected_ids = []
+
+## Write IDs and READ LENGTH to CSV file.
+#paired_sra = open('paired_sra.csv', 'w')
+
+## Iterate through summaries and pick paired end datasets.
+#header = ['SRA_ID', 'ORGANISM', 'NCBI_TAXID', 'STRATEGY', 'PLATFORM', 'TYPE',
+          #'READ_LENGTH', 'READS', 'SPOTS', 'BASES']
+
+#paired_sra.write(', '.join(header) + '\n')
+#print('\n' + '\t '.join(header))
+#for k, v in sra_summaries.iteritems():
+
+    ## Pattern to match: <PAIRED NOMINAL_LENGTH="200"
+    #summary_string = v[0]['ExpXml']
+
+    ## Extract fields from record summary.
+    #fields = extract_summary(summary_string)
+
+    #sra_selected_ids.append(k)
+    #row = [k, fields['organism'], fields['ncbi_taxid'], fields['strategy'],
+            #fields['platform'], fields['type'] ,fields['length'],
+            #fields['reads'], fields['spots'], fields['spots']]
+    #if fields['type'] == 'PAIRED':
+        #paired_sra.write(', '.join(row) + '\n')
+    #print('\t '.join(row))
+
+## Closes CSV file.
+#paired_sra.close()
+
+## Just count selected IDs.
+#count_sra_selected_ids = len(sra_selected_ids)
+
+#print('\nTotal of %d selected IDs. %d%% of returned entries.' % (count_sra_selected_ids, count_sra_selected_ids / int(search_records['Count'])))
