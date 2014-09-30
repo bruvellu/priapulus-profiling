565f8829e48fa58f3a9e01ecc7a8a1faffd8342e
Bruno Vellutini <organelas@gmail.com>
2014-09-30 23:40:23 +0200

    Updates read count script

--- 00_map_to_reference.sh
+++ 00_map_to_reference.sh
@@ -8,16 +8,16 @@ set -e
 wait
 
 # Map each sample to reference:
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_0d.fastq.gz -S mapped_reads/Pc1_0d_bowtie.sam > mapped_reads/Pc1_0d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_1d.fastq.gz -S mapped_reads/Pc1_1d_bowtie.sam > mapped_reads/Pc1_1d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_3d.fastq.gz -S mapped_reads/Pc1_3d_bowtie.sam > mapped_reads/Pc1_3d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_5d.fastq.gz -S mapped_reads/Pc1_5d_bowtie.sam > mapped_reads/Pc1_5d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_0d.fastq.gz -S mapped_reads/Pc1_0d.sam > mapped_reads/Pc1_0d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_1d.fastq.gz -S mapped_reads/Pc1_1d.sam > mapped_reads/Pc1_1d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_3d.fastq.gz -S mapped_reads/Pc1_3d.sam > mapped_reads/Pc1_3d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_5d.fastq.gz -S mapped_reads/Pc1_5d.sam > mapped_reads/Pc1_5d.out 2>&1 &
 wait
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_9d.fastq.gz -S mapped_reads/Pc1_9d_bowtie.sam > mapped_reads/Pc1_9d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_0d.fastq.gz -S mapped_reads/Pc2_0d_bowtie.sam > mapped_reads/Pc2_0d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_1d.fastq.gz -S mapped_reads/Pc2_1d_bowtie.sam > mapped_reads/Pc2_1d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_3d.fastq.gz -S mapped_reads/Pc2_3d_bowtie.sam > mapped_reads/Pc2_3d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc1_9d.fastq.gz -S mapped_reads/Pc1_9d.sam > mapped_reads/Pc1_9d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_0d.fastq.gz -S mapped_reads/Pc2_0d.sam > mapped_reads/Pc2_0d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_1d.fastq.gz -S mapped_reads/Pc2_1d.sam > mapped_reads/Pc2_1d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_3d.fastq.gz -S mapped_reads/Pc2_3d.sam > mapped_reads/Pc2_3d.out 2>&1 &
 wait
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_5d.fastq.gz -S mapped_reads/Pc2_5d_bowtie.sam > mapped_reads/Pc2_5d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_7d.fastq.gz -S mapped_reads/Pc2_7d_bowtie.sam > mapped_reads/Pc2_7d.out 2>&1 &
-/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_9d.fastq.gz -S mapped_reads/Pc2_9d_bowtie.sam > mapped_reads/Pc2_9d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_5d.fastq.gz -S mapped_reads/Pc2_5d.sam > mapped_reads/Pc2_5d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_7d.fastq.gz -S mapped_reads/Pc2_7d.sam > mapped_reads/Pc2_7d.out 2>&1 &
+/usr/local/src/bowtie2-2.2.3/bowtie2 --phred33 --very-sensitive-local -a -p 8 -x reference/Pc_ref -U data/Pc2_9d.fastq.gz -S mapped_reads/Pc2_9d.sam > mapped_reads/Pc2_9d.out 2>&1 &
--- 01_calculate_read_count.sh
+++ 01_calculate_read_count.sh
@@ -1,13 +1,16 @@
-# Calculate read count per transcript by using Bowtie mapping.
-./bowtie_map_to_counts.py mapped_reads/Pc1_0d_bowtie.map > read_count_per_transcript/Pc1_0d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc1_1d_bowtie.map > read_count_per_transcript/Pc1_1d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc1_3d_bowtie.map > read_count_per_transcript/Pc1_3d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc1_5d_bowtie.map > read_count_per_transcript/Pc1_5d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc1_9d_bowtie.map > read_count_per_transcript/Pc1_9d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc2_0d_bowtie.map > read_count_per_transcript/Pc2_0d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc2_1d_bowtie.map > read_count_per_transcript/Pc2_1d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc2_3d_bowtie.map > read_count_per_transcript/Pc2_3d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc2_5d_bowtie.map > read_count_per_transcript/Pc2_5d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc2_7d_bowtie.map > read_count_per_transcript/Pc2_7d.counts &
-./bowtie_map_to_counts.py mapped_reads/Pc2_9d_bowtie.map > read_count_per_transcript/Pc2_9d.counts &
+#!/bin/bash
 
+set -e
+
+# Calculate read count per transcript by using Bowtie mapping.
+python bowtie_map_to_counts.py mapped_reads/Pc1_0d.sam > read_count_per_transcript/Pc1_0d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc1_1d.sam > read_count_per_transcript/Pc1_1d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc1_3d.sam > read_count_per_transcript/Pc1_3d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc1_5d.sam > read_count_per_transcript/Pc1_5d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc1_9d.sam > read_count_per_transcript/Pc1_9d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc2_0d.sam > read_count_per_transcript/Pc2_0d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc2_1d.sam > read_count_per_transcript/Pc2_1d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc2_3d.sam > read_count_per_transcript/Pc2_3d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc2_5d.sam > read_count_per_transcript/Pc2_5d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc2_7d.sam > read_count_per_transcript/Pc2_7d.counts &
+python bowtie_map_to_counts.py mapped_reads/Pc2_9d.sam > read_count_per_transcript/Pc2_9d.counts &
