8aca9bb5e148185afd5b3d005ce4f6545fc35ae0
Bruno Vellutini <organelas@gmail.com>
2014-10-01 22:20:25 +0200

    Adds edgeR installation calls

--- main.sh
+++ main.sh
@@ -3,6 +3,14 @@
 # TODO Check if necessary components are installed.
 # bowtie2, R, edgeR.
 
+# edgeR
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("edgeR")
+#Using Bioconductor version 2.14 (BiocInstaller 1.14.2), R version
+#  3.1.1.
+#trying URL 'http://bioconductor.org/packages/2.14/bioc/src/contrib/limma_3.20.9.tar.gz'
+#trying URL 'http://bioconductor.org/packages/2.14/bioc/src/contrib/edgeR_3.6.8.tar.gz'
+
 sh 00_map_to_reference.sh
 sh 01_calculate_read_count.sh
 sh 02_import_to_r.sh
