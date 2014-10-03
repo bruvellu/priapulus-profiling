0b972a799cda69ebc5aeee9bc07af013e3ebfbce
Bruno Vellutini <organelas@gmail.com>
2014-10-03 14:25:44 +0200

    Shows x scale in all plots

--- de_plot.r
+++ de_plot.r
@@ -25,7 +25,7 @@ de_plots <- function(df, de_counts, filepath) {
     points(m1[m1$PAdjust_0d_1d < .05,]$logCPM_0d_1d, m1[m1$PAdjust_0d_1d <  .05,]$logFC_0d_1d, pch=16, col=rgb(255,0,0,70,maxColorValue=255))
     abline(h=c(-1,1), col="grey")
 
-    plot(m2$logCPM_1d_3d, m2$logFC_1d_3d, main="C", pch=16, ylim=c(-11,14), xlim=c(-3,18), col=rgb(0,0,0,40,maxColorValue=255), xaxt="n", ylab = "", xlab = "")
+    plot(m2$logCPM_1d_3d, m2$logFC_1d_3d, main="C", pch=16, ylim=c(-11,14), xlim=c(-3,18), col=rgb(0,0,0,40,maxColorValue=255), ylab = "", xlab = "")
     points(m2[m2$PAdjust_1d_3d < .05,]$logCPM_1d_3d, m2[m2$PAdjust_1d_3d <  .05,]$logFC_1d_3d, pch=16, col=rgb(255,0,0,70,maxColorValue=255))
     abline(h=c(-1,1), col="grey")
 
@@ -33,11 +33,11 @@ de_plots <- function(df, de_counts, filepath) {
     points(m3[m3$PAdjust_3d_5d < .05,]$logCPM_3d_5d, m3[m3$PAdjust_3d_5d <  .05,]$logFC_3d_5d, pch=16, col=rgb(255,0,0,70,maxColorValue=255))
     abline(h=c(-1,1), col="grey")
 
-    plot(m4$logCPM_5d_7d, m4$logFC_5d_7d, main="E", pch=16, ylim=c(-11,14),xlim=c(-3,18), col=rgb(0,0,0,40,maxColorValue=255), xaxt="n", ylab = "", xlab = "")
+    plot(m4$logCPM_5d_7d, m4$logFC_5d_7d, main="E", pch=16, ylim=c(-11,14),xlim=c(-3,18), col=rgb(0,0,0,40,maxColorValue=255), ylab = "", xlab = "")
     points(m4[m4$PAdjust_5d_7d < .05,]$logCPM_5d_7d, m4[m4$PAdjust_5d_7d <  .05,]$logFC_5d_7d, pch=16, col=rgb(255,0,0,70,maxColorValue=255))
     abline(h=c(-1,1), col="grey")
 
-    plot(m5$logCPM_7d_9d, m5$logFC_7d_9d, main="F", pch=16, ylim=c(-11,14),xlim=c(-3,18), col=rgb(0,0,0,40,maxColorValue=255), xaxt="n", ylab = "", xlab = "")
+    plot(m5$logCPM_7d_9d, m5$logFC_7d_9d, main="F", pch=16, ylim=c(-11,14),xlim=c(-3,18), col=rgb(0,0,0,40,maxColorValue=255), ylab = "", xlab = "")
     points(m5[m5$PAdjust_7d_9d < .05,]$logCPM_7d_9d, m5[m5$PAdjust_7d_9d <  .05,]$logFC_7d_9d, pch=16, col=rgb(255,0,0,70,maxColorValue=255))
     abline(h=c(-1,1), col="grey")
 
